948b1a9b6c039388de05f1dc636bffdc16a4f23d
Mark <markgabon@hotmail.com>
2022-02-18 14:40:48 -0500

    Initial commit

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here and require it from index.js
+const express = require('express')
+const server = express()
+
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
+
+server.use(express.json())
+
+server.use('/api/project', projectRouter)
+server.use('/api/resource', resourceRouter)
+server.use('/api/task', taskRouter)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Lets write some middleware!</h2>`)
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // start your server here
+require('dotenv').config()
+const server = require('./api/server.js')
+
+const port = process.env.PORT || 9000
+
+server.listen(port, () => {
+    console.log(`API Running on ${port}`)
+})
